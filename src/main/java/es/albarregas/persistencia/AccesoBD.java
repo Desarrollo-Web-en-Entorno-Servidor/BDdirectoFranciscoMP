8868c5202d167c8db80c8008371d7a2768eeec86
fmurillopacheco <fmurilop@alumnos.unex.es>
2018-10-25 01:19:11 +0200

    Subida de archivos  proyecto DBdirectoFranciscoMP. Pendiente README

--- src/main/java/es/albarregas/persistencia/AccesoBD.java
+++ src/main/java/es/albarregas/persistencia/AccesoBD.java
@@ -5,17 +5,26 @@
  */
 package es.albarregas.persistencia;
 
+import static com.sun.corba.se.spi.presentation.rmi.StubAdapter.request;
+import es.albarregas.beans.Ave;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 
 /**
  *
@@ -76,74 +85,97 @@ public class AccesoBD extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        
         processRequest(request, response);
+        Connection conexion = null;
+        Statement sentencia = null;
+        PreparedStatement preparada = null;
+        ResultSet resultado = null;
+        Ave ave = null;
+        List<Ave> listado = null;
+
+        String anilla = request.getParameter("anilla");
+        String sql = null;
+        String url = null;
+        
+
+        
+        
+        
+        try {
+            //Carga el controlador para acceder a la BD.
+            Class.forName("com.mysql.jdbc.Driver");
+            //Realiza la conexión.
+            conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/pruebasjava","java2019","2019");
+            sentencia = conexion.createStatement();
+            
+            if (request.getParameter("unaAnilla") != null) {
+
+                sql = "select * from aves where anilla = ?";
+                preparada = conexion.prepareStatement(sql);
+                preparada.setString(1, anilla);
+                try {
+                    resultado = preparada.executeQuery();
+
+                    resultado.next();
+                    url = "unResultado.jsp";
+                    request.setAttribute("anilla", resultado.getString("anilla"));
+                    request.setAttribute("especie", resultado.getString("especie"));
+                    request.setAttribute("lugar", resultado.getString("lugar"));
+                    request.setAttribute("fecha", resultado.getString("fecha"));
+                } catch (SQLException e) {
+
+                    url = "error.jsp";
+                    request.setAttribute("error", "La anilla " + anilla
+                            + " no existe en la base de datos");
+                }
+            } else {
+                sql = "select * from aves";
+                sentencia = conexion.createStatement();
+                resultado = sentencia.executeQuery(sql);
+                listado = new ArrayList();
+                url = "listaResultado.jsp";
+
+                while (resultado.next()) {
+
+                    ave = new Ave();
+                    ave.setAnilla(resultado.getString("anilla"));
+                    ave.setEspecie(resultado.getString("especie"));
+                    ave.setLugar(resultado.getString("lugar"));
+                    ave.setFecha(resultado.getString("fecha"));
+                    listado.add(ave);
+                }
+                request.setAttribute("lista", listado);
+            }
+
+            request.getRequestDispatcher(url).forward(request, response);
+        } catch (SQLException ex) {
+            System.out.println("Error al crear la conexión");
+            ex.printStackTrace();
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(AccesoBD.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                if (resultado != null) {
+                    resultado.close();
+                }
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+            try {
+                if (preparada != null) {
+                    preparada.close();
+                }
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+            try {
+                if (conexion != null) {
+                    conexion.close();
+                }
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
     }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    
-    Connection conexion = null;
-    Statement stm = null; // instrucción de consulta
-    ResultSet rset = null; // conjunto de resultados
-    Ave ave=null;
-    List<Ave> aves=null;
-	
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			String BaseDeDatos = "jdbc:mysql://localhost/pruebasjava\",\"root\", \"albarregas\"";
-			conexion = DriverManager.getConnection(BaseDeDatos,"java2019","2019");
-			stm=conexion.createStatement();
-			rset=stm.executeQuery("select * from aves where anilla=?");
-			ResultSetMetaData metaDatos = rset.getMetaData();
-			
-		// Creamos un array unidimensional con los títulos de las columnas
-			Object[] columnas = {metaDatos.getColumnName(1),metaDatos.getColumnName(2),metaDatos.getColumnName(3)};
-			
-			while ( rset.next() ) {
-			// Se añade un elemento <Persona> a la colección por cada fila del resultado
-				personas.add(new Persona(rset.getObject(1).toString(),rset.getObject(2).toString(),rset.getObject(3).toString()));			
-			} // fin de while
-			
-		// Creamos un array bidimensional con los datos de la colección.		
-			Object[][] filas = new Object[personas.size()][3];
-			for ( int x = 0;x < personas.size(); x++ ){
-				filas[x][0]=personas.get(x).getNumero();	
-				filas[x][1]=personas.get(x).getNombre();		
-				filas[x][2]=personas.get(x).getCategoria();					
-			}
-// Se crea objeto de tipo Jtable
-			JTable tabla = new JTable(filas,columnas);
-			
-// Se crea objeto de tipo Vista
-			@SuppressWarnings("unused")
-			Vista miVista = new Vista(tabla);
-		}catch ( SQLException excepcionSql ){
-				System.out.println("Excepción:"+excepcionSql);
-		} // fin de catch
-		catch ( ClassNotFoundException noEncontroClase ){
-			System.out.println("Excepción:"+noEncontroClase);
-		} // fin de catch
-		catch ( Exception excepcion ){
-			System.out.println("Excepción:"+excepcion);
-		} // fin de catch
-		finally{
-			try {
-				rset.close();
-				stm.close();
-				conexion.close();
-			} // fin de try
-			catch ( Exception e ) {
-				System.out.println("Excepción:"+e);
-			} // fin de catch
-		} // fin de finally	
-	}
-    
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
-}
+    }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -9,9 +9,24 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>JSP Page</title>
+        <title>Avistamientos</title>
+        <link rel="stylesheet" type="text/css" href="estilo.css" />
     </head>
     <body>
-        <h1>Hello World!</h1>
+        <div id="contenido">
+            <h2>Página de avistamientos</h2>
+            <form action="buscador" method="post">
+                <table>
+                    <tr>
+                        <td>Introduce una anilla</td>
+                        <td><input name="anilla" /></td>
+                    </tr>
+                    <tr>
+                        <td><input type="submit" value="Anilla" name="unaAnilla"/></td>
+                        <td><input type="submit" value="Todas" name="todas"/></td>
+                    </tr>
+                </table>
+            </form>
+        </div>
     </body>
 </html>
